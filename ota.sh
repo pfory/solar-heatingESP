bb6e3266dd93cc5e3a345c541ef770e175273e44
pfory <pfory@seznam.cz>
2020-09-21 15:32:04 +0200

    uprava ota

--- ota.sh
+++ ota.sh
@@ -2,4 +2,4 @@
 upravit verzi XXXX
 spustit v příkazové řádce
 
-sudo python /root/.arduino15/packages/esp8266/hardware/esp8266/2.4.2/tools/espota.py -i 192.168.1.108 -p 8266 --auth=password -f /home/pi/solar-heating.ino.d1_mini.bin
+sudo python /root/.arduino15/packages/esp8266/hardware/esp8266/2.4.2/tools/espota.py -i 192.168.1.108 -p 8266 --auth=password -f /home/petr/solar-heatingESP.ino.d1_mini.bin
